02e5f2a621d7b12cb3b01a98bb0277b181b184ee
Branden Higby <branden@higby.io>
2025-03-10 17:23:30 -0700

    Update details and accidental lint

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 #!/usr/bin/env node
 
-import boxen from 'boxen'
-import chalk from 'chalk'
+import boxen from "boxen";
+import chalk from "chalk";
 
 let higby = [
   {
-    title: 'www',
-    href: '      https://www.higby.io'
+    title: "www",
+    href: "      https://www.higby.io",
   },
   {
-    title: 'email',
-    href: '    mailto:hi@higby.io'
+    title: "email",
+    href: "    mailto:branden@higby.io",
   },
   {
-    title: 'mastodon',
-    href: ' https://front-end.social/@higby'
-  }
-]
+    title: "mastodon",
+    href: " https://front-end.social/@higby",
+  },
+];
 
 console.log(
-  boxen(higby.map(link => `${chalk.bold(link.title)} ${link.href}`).join('\n'), {
-    padding: 1,
-    title: 'higby',
-    titleAlignment: 'left'
-  })
-)
+  boxen(
+    higby.map((link) => `${chalk.bold(link.title)} ${link.href}`).join("\n"),
+    {
+      padding: 1,
+      title: "branden higby",
+      titleAlignment: "left",
+    }
+  )
+);
